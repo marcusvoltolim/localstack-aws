e66ef37e035296a740aa323013497abacf2053e7
Marcus Voltolim <marcus.voltolim@gmail.com>
2022-05-02 21:37:12 -0300

    initial commit, creating gradle project

---
+++ src/main/groovy/io/marcusvoltolim/examples/localstack/LocalstackApplication.groovy
@@ -0,0 +1,13 @@
+package io.marcusvoltolim.examples.localstack
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LocalstackApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LocalstackApplication, args)
+    }
+
+}
---
+++ src/test/groovy/io/marcusvoltolim/examples/localstack/LocalstackApplicationTests.groovy
@@ -0,0 +1,13 @@
+package io.marcusvoltolim.examples.localstack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LocalstackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
